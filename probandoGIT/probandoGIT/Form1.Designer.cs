ce4b993e1b3f88f32d035443fda09388d4038c43
Josue Hernandez <josue200014@outlook.com>
2021-01-11 15:32:27 -0600

    boton agregado

--- probandoGIT/probandoGIT/Form1.Designer.cs
+++ probandoGIT/probandoGIT/Form1.Designer.cs
@@ -33,12 +33,13 @@ namespace probandoGIT
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(245, 135);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(68, 42);
             this.button1.TabIndex = 0;
@@ -59,7 +60,7 @@ namespace probandoGIT
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(260, 232);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(68, 28);
             this.button2.TabIndex = 2;
@@ -75,16 +76,26 @@ namespace probandoGIT
             this.label2.TabIndex = 3;
             this.label2.Text = "BRAYAN 2";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(463, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -98,6 +109,7 @@ namespace probandoGIT
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
