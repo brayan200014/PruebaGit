9b75068143893577e68c930414be0adb3a2854e3
Josue Hernandez <josue200014@outlook.com>
2021-01-11 15:06:09 -0600

    mensaje guardado

--- probandoGIT/probandoGIT/Form1.Designer.cs
+++ probandoGIT/probandoGIT/Form1.Designer.cs
@@ -36,9 +36,10 @@ namespace probandoGIT
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(327, 166);
+            this.button1.Location = new System.Drawing.Point(245, 135);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 52);
+            this.button1.Size = new System.Drawing.Size(68, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,29 +48,32 @@ namespace probandoGIT
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(269, 65);
+            this.label1.Location = new System.Drawing.Point(202, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Soy Brayan";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(347, 285);
+            this.button2.Location = new System.Drawing.Point(260, 232);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 35);
+            this.button2.Size = new System.Drawing.Size(68, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
